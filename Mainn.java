09be3518810229e34cc7ea7b293f392986274282
Ipekyurttas <129221186+Ipekyurttas@users.noreply.github.com>
2024-04-03 19:57:09 +0300

    Add files via upload

---
+++ Mainn.java
@@ -0,0 +1,39 @@
+
+package odev1;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Mainn {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Bir cumle giriniz : ");
+        String cumle = input.nextLine();
+        System.out.println("Girilen cumle : "+cumle);
+        ArrayList<String> kelime = new ArrayList<>();
+        StringTokenizer tokenizer = new StringTokenizer(cumle);
+        while(tokenizer.hasMoreTokens()){
+            kelime.add(tokenizer.nextToken());
+        }
+        int i = 0, kontrol = 0;
+        Methodlar mth = new Methodlar();
+        int boyut = kelime.size();
+        if (boyut == 3) {
+            kontrol += mth.ozne(kelime.get(i));
+            i++;
+            kontrol += mth.nesne(kelime.get(i));
+            i++;
+            kontrol += mth.yüklem(kelime.get(i));
+            if (kontrol == 1) {
+                System.out.println("EVET");
+            } else {
+                System.out.println("HAYIR");
+            }
+        } else {
+            System.out.println("HAYIR");
+        }
+    }
+    
+}
